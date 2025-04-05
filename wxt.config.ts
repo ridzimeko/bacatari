6e910329bd059dc421095ee51bf439cbb526dd43
Amro Alfien <ridzimeko@proton.me>
2025-04-05 14:02:40 +0700

    chore: change directory to /src & update manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: './src',
   modules: ['@wxt-dev/module-vue'],
+  manifest: {
+    name: 'Bacatari',
+    description: 'Baca berita tanpa ribet',
+    version: '1.0.0',
+    permissions: ['tabs', 'webRequest', 'webRequestBlocking', 'declarativeNetRequest', 'activeTab'],
+    host_permissions: ['*://*/*']
+  }
 });
