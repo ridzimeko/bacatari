6f39e6c2779bb503cf6d1a7e0c5ea94d1720f59b
Amro Alfien <ridzimeko@proton.me>
2025-04-06 16:07:33 +0700

    feat: anti clipboard
    prevent site to modify/sabotage user clipboard

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,6 +1,21 @@
+import websites from "@/filters/websites";
+import applyAntiClipboard from "@/utils/antiClipboard";
+
 export default defineContentScript({
   matches: ['<all_urls>'],
+  runAt: 'document_start',
   main() {
-    console.log('Hello content.');
+    for (const website of websites) {
+      if (location.hostname.includes(website.domain)) {
+
+        // protect clipboard
+        if (document.readyState === "loading") {
+          document.addEventListener("DOMContentLoaded", applyAntiClipboard);
+        } else {
+          applyAntiClipboard();
+        }
+
+      }
+    }
   },
 });
---
+++ src/utils/antiClipboard.ts
@@ -0,0 +1,19 @@
+const applyAntiClipboard = () => {
+    // Bersihin handler .oncopy
+    document.oncopy = null;
+
+    // Pasang listener yang nutup semua sabotase
+    document.addEventListener("copy", (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        const selectedText = window.getSelection() as Selection;
+        if (selectedText.toString().length > 0 && navigator.clipboard) {
+            navigator.clipboard.writeText(selectedText.toString());
+        }
+    }, true);
+
+    console.log('anti clipboard applied');
+};
+
+export default applyAntiClipboard;
