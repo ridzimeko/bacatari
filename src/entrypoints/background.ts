366a97d4d83f7b2e62655befe5dda8c0a08fe3e1
Amro Alfien <ridzimeko@proton.me>
2025-04-06 14:37:31 +0700

    feat: add `?page=all` url params
    this will make article to be single page

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,3 +1,23 @@
+import websites from "@/filters/websites";
+
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
+  browser.webRequest.onBeforeRequest.addListener(
+    (details) => {
+      const url = new URL(details.url);
+
+      for (const website of websites) {
+        if (url.hostname.includes(website.domain)) {
+
+          // add ?page=all to the matched url
+          if (url.pathname.match(website.articlePath) && !url.searchParams.has('page')) {
+            url.searchParams.set('page', 'all');
+            return { redirectUrl: url.toString() };
+          }
+
+        }
+      }
+    },
+    { urls: ['*://*/*'], types: ['main_frame'] },
+    ['blocking']
+  );
+})
---
+++ src/filters/websites.ts
@@ -0,0 +1,32 @@
+const websites = [
+    {
+        domain: "detik.com",
+        articlePath: /\/[a-z\-]+\/d-\d+\/.+/,
+    },
+    {
+        domain: "kompas.com",
+        articlePath: /\/read\/\d{4}\/\d{2}\/\d{2}\/.+/,
+    },
+    {
+        domain: "cnnindonesia.com",
+        articlePath: /\/[a-z\-]+\/[\d\-]+\/.+/,
+    },
+    {
+        domain: "tribunnews.com",
+        articlePath: /\/[a-z\-]+\/\d{4}\/\d{2}\/\d{2}\/.+/,
+    },
+    {
+        domain: "liputan6.com",
+        articlePath: /\/[a-z\-]+\/read\/\d+\/.+/,
+    },
+    {
+        domain: "suara.com",
+        articlePath: /\/[a-z\-]+\/\d{4}\/\d{2}\/\d{2}\/.+/,
+    },
+    {
+        domain: "antaranews.com",
+        articlePath: /\/(berita|infografik)\/\d+\/.+/,
+    },
+];
+
+export default websites;
