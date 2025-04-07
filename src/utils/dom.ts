d50a636f9a14269ba53996fb363fe139e22fb1bc
Amro Alfien <ridzimeko@proton.me>
2025-04-07 16:10:05 +0700

    feat: redirect Medium article to Freedium button

---
+++ src/utils/dom.ts
@@ -0,0 +1,17 @@
+export function waitForElement(selector: string, callback: (el: Element) => void) {
+    const target = document.querySelector(selector);
+    if (target) return callback(target);
+
+    const observer = new MutationObserver(() => {
+        const el = document.querySelector(selector);
+        if (el) {
+            observer.disconnect();
+            callback(el);
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
---
+++ src/utils/freeMedium.ts
@@ -0,0 +1,7 @@
+export default function freeMedium(url: URL) {
+    const originalHost = url.host;
+    const originalPath = url.pathname;
+    const redirectUrl = `https://freedium.cfd/${originalHost}${originalPath}`;
+
+    window.location.href = redirectUrl;
+}
